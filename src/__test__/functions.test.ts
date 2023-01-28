776ddcf030c12f845d3e0983f5fe80237d1e2f80
Max Edlund <maxedlund88@gmail.com>
2023-01-28 20:10:11 +0100

    add test func for removeAllTodos

--- src/__test__/functions.test.ts
+++ src/__test__/functions.test.ts
@@ -1,4 +1,4 @@
-import { changeTodo } from "../ts/functions";
+import { changeTodo, removeAllTodos } from "../ts/functions";
 import { Todo } from "../ts/models/Todo"
 
 test('should change todo done status', () => {
@@ -7,4 +7,20 @@ test('should change todo done status', () => {
   changeTodo(todo);
 
   expect(todo.done).toBeTruthy();
+})
+
+test('should remove all todos from array', () => {
+  let todoList: Todo[] = [];
+
+  for (let i = 0; i < 3; i++) {
+    let todo: Todo = new Todo('example todo', false);
+    todoList.push(todo)
+  }
+
+  let originalLength = todoList.length;
+
+  removeAllTodos(todoList);
+
+  expect(originalLength).toBe(3);
+  expect(todoList.length).toBe(0);
 })
